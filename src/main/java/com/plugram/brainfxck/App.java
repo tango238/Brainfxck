cfa5b1f6a5345414f3ecffe4ed108a9b4996a8e2
Go Tanaka <tanago3@gmail.com>
2012-10-07 13:31:58 +0900

    initial commit.

---
+++ src/main/java/com/plugram/brainfxck/App.java
@@ -0,0 +1,13 @@
+package com.plugram.brainfxck;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
